a59bbc1c34659dba166a849b9780132b8ea15726
Derek Collison <derek.collison@gmail.com>
2012-10-05 07:40:22 -0700

    Bumped version for EncodedConn.Request()

--- nats.go
+++ nats.go
@@ -25,7 +25,7 @@ import (
 )
 
 const (
-	Version              = "0.5"
+	Version              = "0.52"
 	DefaultURL           = "nats://localhost:4222"
 	DefaultPort          = 4222
 	DefaultMaxReconnect  = 10
