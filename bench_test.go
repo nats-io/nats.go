ee42b00b7f7fe2d389162b38e65deb616e2cd6bb
Derek Collison <derek.collison@gmail.com>
2012-12-17 15:52:21 -0800

    Added PubSub benchmark

--- bench_test.go
+++ bench_test.go
@@ -2,6 +2,7 @@ package nats
 
 import (
 	"testing"
+	"time"
 )
 
 func BenchmarkPublishSpeed(b *testing.B) {
@@ -9,19 +10,72 @@ func BenchmarkPublishSpeed(b *testing.B) {
 	server := startServer(b, DefaultPort, "")
 	defer server.stopServer()
 	nc, err := Connect(DefaultURL)
+	if err != nil {
+		b.Fatalf("Could not connect: %v\n", err)
+	}
 	defer nc.Close()
 	b.StartTimer()
+	b.SetBytes(1)
 
 	msg := []byte("Hello World")
 
-	for i := 1; i < b.N; i++ {
+	for i := 0; i < b.N; i++ {
 		err = nc.Publish("foo", msg)
 		if err != nil {
 			b.Fatalf("Error in benchmark during Publish: %v\n", err)
 		}
-		b.SetBytes(1)
 	}
 	// Make sure they are all processed.
 	nc.Flush()
+	b.StopTimer()
+}
+
+func BenchmarkPubSubSpeed(b *testing.B) {
+	b.StopTimer()
+	server := startServer(b, DefaultPort, "")
+	defer server.stopServer()
+	nc, err := Connect(DefaultURL)
+	if err != nil {
+		b.Fatalf("Could not connect: %v\n", err)
+	}
+	defer nc.Close()
+
+	ch := make(chan bool)
+	b.StartTimer()
+	b.SetBytes(2)
+
+	nc.Opts.AsyncErrorCB = func(nc *Conn, s *Subscription, err error) {
+		b.Fatalf("Error : %v\n", err)
+	}
+
+	received := 0
+
+	nc.Subscribe("foo", func(m *Msg) {
+		received += 1
+		if received >= b.N {
+			ch <- true
+		}
+	})
+
+	msg := []byte("Hello World")
+
+	for i := 0; i < b.N; i++ {
+		err = nc.Publish("foo", msg)
+		if err != nil {
+			b.Fatalf("Error in benchmark during Publish: %v\n", err)
+		}
+		// Don't overrun ourselves and be a slow consumer
+		if i % 1000 == 0 {
+			time.Sleep(10)
+		}
+	}
+
+	// Make sure they are all processed.
+	err = waitTime(ch, 10*time.Second)
+	if err != nil {
+		b.Fatal("Timed out waiting for messages")
+	} else if received != b.N {
+		b.Fatal(nc.LastError())
+	}
 	b.StopTimer()
 }
