21172c9a568189d2461c16e76c32c77aa0977895
Ken Robertson <ken@invalidlogic.com>
2013-05-30 23:11:54 -0700

    Revert "Remove examples"
    This reverts commit 6a934388ea23c02171ce81cc6624a53f16fabedc.

---
+++ examples/nats-pub.go
@@ -0,0 +1,44 @@
+// Copyright 2012 Apcera Inc. All rights reserved.
+// +build ignore
+
+package main
+
+import (
+	"flag"
+	"github.com/apcera/nats"
+	"log"
+)
+
+func usage() {
+	log.Fatalf("Usage: nats-pub [-s server] [--ssl] [-t] <subject> <msg> \n")
+}
+
+func main() {
+	var url = flag.String("s", nats.DefaultURL, "The nats server URL")
+	var ssl = flag.Bool("ssl", false, "Use Secure Connection")
+
+	log.SetFlags(0)
+	flag.Usage = usage
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 1 {
+		usage()
+	}
+
+	opts := nats.DefaultOptions
+	opts.Url = *url
+	opts.Secure = *ssl
+
+	nc, err := opts.Connect()
+	if err != nil {
+		log.Fatalf("Can't connect: %v\n", err)
+	}
+
+	subj, msg := args[0], []byte(args[1])
+
+	nc.Publish(subj, msg)
+	nc.Close()
+
+	log.Printf("Published [%s] : '%s'\n", subj, msg)
+}
---
+++ examples/nats-sub.go
@@ -0,0 +1,58 @@
+// Copyright 2012 Apcera Inc. All rights reserved.
+// +build ignore
+
+package main
+
+import (
+	"flag"
+	"github.com/apcera/nats"
+	"log"
+	"runtime"
+)
+
+func usage() {
+	log.Fatalf("Usage: nats-sub [-s server] [--ssl] [-t] <subject> \n")
+}
+
+var index = 0
+
+func printMsg(m *nats.Msg, i int) {
+	index += 1
+	log.Printf("[#%d] Received on [%s]: '%s'\n", i, m.Subject, string(m.Data))
+}
+
+func main() {
+	var url = flag.String("s", nats.DefaultURL, "The nats server URL")
+	var showTime = flag.Bool("t", false, "Display timestamps")
+	var ssl = flag.Bool("ssl", false, "Use Secure Connection")
+
+	log.SetFlags(0)
+	flag.Usage = usage
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 1 {
+		usage()
+	}
+
+	opts := nats.DefaultOptions
+	opts.Url = *url
+	opts.Secure = *ssl
+
+	nc, err := opts.Connect()
+	if err != nil {
+		log.Fatalf("Can't connect: %v\n", err)
+	}
+
+	subj, i := args[0], 0
+
+	nc.Subscribe(subj, func(msg *nats.Msg) {
+		i += 1
+		printMsg(msg, i)
+	})
+
+	log.Printf("Listening on [%s]\n", subj)
+	if *showTime { log.SetFlags(log.LstdFlags) }
+
+	runtime.Goexit()
+}
