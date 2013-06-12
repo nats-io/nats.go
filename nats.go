f73af30b3584036e064a73b037488c80b935ada6
Derek Collison <derek.collison@gmail.com>
2013-06-11 21:41:13 -0700

    Bumped version

--- nats.go
+++ nats.go
@@ -28,7 +28,7 @@ import (
 )
 
 const (
-	Version              = "0.83"
+	Version              = "0.84"
 	DefaultURL           = "nats://localhost:4222"
 	DefaultPort          = 4222
 	DefaultMaxReconnect  = 10
