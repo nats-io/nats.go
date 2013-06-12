e8a032089580002442229bc9f535b4e55aa11b52
Derek Collison <derek.collison@gmail.com>
2013-06-12 00:19:48 -0700

    Updated copyright

--- parser.go
+++ parser.go
@@ -1,4 +1,4 @@
-// Copyright 2012 Apcera Inc. All rights reserved.
+// Copyright 2012-2013 Apcera Inc. All rights reserved.
 
 // A Go client for the NATS messaging system (https://github.com/derekcollison/nats).
 package nats
