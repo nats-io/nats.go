5bda817aea2c20a80195b842a3a6eebc861b1d0d
Derek Collison <derek.collison@gmail.com>
2015-09-17 23:18:26 -0700

    select directories only

--- travis/coveralls-script.sh
+++ travis/coveralls-script.sh
@@ -1,15 +1,12 @@
 #!/bin/bash -e
 echo "mode: count" > acc.out
-for Dir in $(find ./* -maxdepth 10 -type d | grep -v test);
+for Dir in . ./encoders/builtin ./encoders/protobuf ./test
 do
-        if ls $Dir/*.go &> /dev/null;
-        then
-            GOMAXPROCS=1 go test -v -covermode=count -coverprofile=profile.out $Dir
-            if [ -f profile.out ]
-            then
-                cat profile.out | grep -v "mode: count" >> acc.out
-            fi
-fi
+    go test -v -covermode=count -coverprofile=profile.out $Dir
+    if [ -f profile.out ]
+    then
+	cat profile.out | grep -v "mode: count" >> acc.out
+    fi
 done
 $HOME/gopath/bin/goveralls -coverprofile=acc.out $COVERALLS
 rm -rf ./profile.out
