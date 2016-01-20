e738b000bc359fa849ce381b10c8d0f5c90e8eba
anatolii.prylutskyi <anatolii.prylutskyi@sintez.co.za>
2016-01-20 15:08:08 +0200

    Remove minor comments

--- enc_test.go
+++ enc_test.go
@@ -28,17 +28,13 @@ func TestPublishErrorAfterSubscribeDecodeError(t *testing.T) {
 	nc, _ := opts.Connect()
 	defer nc.Close()
 	c, _ := NewEncodedConn(nc, JSON_ENCODER)
-
 	type Message struct {
 		Message string
 	}
 	const testSubj = "test"
 
 	c.Subscribe(testSubj, func(msg *Message) {})
-
-	//Sending invalid json message
 	c.Publish(testSubj, `foo`)
-
 	c.Flush()
 	if err := c.Publish(testSubj, Message{"2"}); err != nil {
 		t.Error("Fail to send correct json message after decode error in subscription")
@@ -52,12 +48,9 @@ func TestPublishErrorAfterInvalidPublishMessage(t *testing.T) {
 	nc, _ := opts.Connect()
 	defer nc.Close()
 	c, _ := NewEncodedConn(nc, protobuf.PROTOBUF_ENCODER)
-
 	const testSubj = "test"
 
 	c.Publish(testSubj, &testdata.Person{Name: "Anatolii"})
-
-	//Sending invalid protobuf message
 	c.Publish(testSubj, "foo")
 	if err := c.Publish(testSubj, &testdata.Person{Name: "Anatolii"}); err != nil {
 		t.Error("Fail to send correct json message after invalid message publishing", err)
