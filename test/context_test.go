b19224167e89e0d1f3dca05cdc874c1d62e9cb96
Waldemar Quevedo <wally@synadia.com>
2018-08-06 11:05:19 -0700

    Fix for flapping context tests
    Some of the tests assumed that the cancellation was already
    propagated so were failing occasionally:
    
    ```
    --- FAIL: TestOldContextRequestWithTimeoutCanceled (0.03s)
    	context_test.go:141: Expected request with timeout context to fail: %!s(<nil>)
    ```
    
    In order to prevent this, added an extra wait to the context tests
    and wait for context to be detected as cancelled.
    
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>
    
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>
    
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>

--- test/context_test.go
+++ test/context_test.go
@@ -17,6 +17,7 @@ package test
 
 import (
 	"context"
+	"errors"
 	"strings"
 	"testing"
 	"time"
@@ -136,6 +137,24 @@ func testContextRequestWithTimeoutCanceled(t *testing.T, nc *nats.Conn) {
 	// Cancel the context already so that rest of requests fail.
 	cancelCB()
 
+	// Wait a bit for context to be eventually canceled.
+	errCh := make(chan error)
+	tf := time.AfterFunc(50*time.Millisecond, func() {
+		errCh <- errors.New("Timeout waiting for context to be canceled")
+	})
+
+WaitForCancellation:
+	for range time.NewTicker(1 * time.Millisecond).C {
+		select {
+		case <-ctx.Done():
+			tf.Stop()
+			break WaitForCancellation
+		case err := <-errCh:
+			t.Fatal(err)
+		}
+	}
+
+	// Context is already canceled so requests should immediately fail.
 	_, err = nc.RequestWithContext(ctx, "fast", []byte("world"))
 	if err == nil {
 		t.Fatal("Expected request with timeout context to fail")
