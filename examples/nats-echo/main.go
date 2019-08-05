86a3b8e6c272b35bb0ad337acc9508140f9db5b9
kaxapi <kashimov@gmail.com>
2019-08-06 01:27:46 +0600

    Replace usage of deprecated DisconnectHandler function with DisconnectErrHandler

--- examples/nats-echo/main.go
+++ examples/nats-echo/main.go
@@ -134,9 +134,9 @@ func setupConnOptions(opts []nats.Option) []nats.Option {
 
 	opts = append(opts, nats.ReconnectWait(reconnectDelay))
 	opts = append(opts, nats.MaxReconnects(int(totalWait/reconnectDelay)))
-	opts = append(opts, nats.DisconnectHandler(func(nc *nats.Conn) {
+	opts = append(opts, nats.DisconnectErrHandler(func(nc *nats.Conn, err error) {
 		if !nc.IsClosed() {
-			log.Printf("Disconnected: will attempt reconnects for %.0fm", totalWait.Minutes())
+			log.Printf("Disconnected due to: %s, will attempt reconnects for %.0fm", err, totalWait.Minutes())
 		}
 	}))
 	opts = append(opts, nats.ReconnectHandler(func(nc *nats.Conn) {
