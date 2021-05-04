8dd5ec6c97bccaf8fc09b45a76d5694f0f6ae403
Ivan Kozlovic <ivan@synadia.com>
2021-05-03 18:51:58 -0600

    [ADDED] `--nkey` flag to enable nkey authentication using go examples
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- examples/nats-req/main.go
+++ examples/nats-req/main.go
@@ -1,4 +1,4 @@
-// Copyright 2012-2019 The NATS Authors
+// Copyright 2012-2021 The NATS Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -27,7 +27,7 @@ import (
 // nats-req -s demo.nats.io:4443 <subject> <msg> (TLS version)
 
 func usage() {
-	log.Printf("Usage: nats-req [-s server] [-creds file] <subject> <msg>\n")
+	log.Printf("Usage: nats-req [-s server] [-creds file] [-nkey file] <subject> <msg>\n")
 	flag.PrintDefaults()
 }
 
@@ -39,6 +39,7 @@ func showUsageAndExit(exitcode int) {
 func main() {
 	var urls = flag.String("s", nats.DefaultURL, "The nats server URLs (separated by comma)")
 	var userCreds = flag.String("creds", "", "User Credentials File")
+	var nkeyFile = flag.String("nkey", "", "NKey Seed File")
 	var showHelp = flag.Bool("h", false, "Show help message")
 
 	log.SetFlags(0)
@@ -57,9 +58,22 @@ func main() {
 	// Connect Options.
 	opts := []nats.Option{nats.Name("NATS Sample Requestor")}
 
+	if *userCreds != "" && *nkeyFile != "" {
+		log.Fatal("specify -seed or -creds")
+	}
+
 	// Use UserCredentials
 	if *userCreds != "" {
 		opts = append(opts, nats.UserCredentials(*userCreds))
+	}
+
+	// Use Nkey authentication.
+	if *nkeyFile != "" {
+		opt, err := nats.NkeyOptionFromSeed(*nkeyFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		opts = append(opts, opt)
 	}
 
 	// Connect to NATS
