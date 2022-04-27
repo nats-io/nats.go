a5e2bcfe64f7908da561ac4ea4f1417e8e87eaf8
Ludovico Russo <ludus.russo@gmail.com>
2022-04-27 14:57:44 +0200

    feat: add UpsertStream function

--- jsm.go
+++ jsm.go
@@ -30,6 +30,9 @@ type JetStreamManager interface {
 	// UpdateStream updates a stream.
 	UpdateStream(cfg *StreamConfig, opts ...JSOpt) (*StreamInfo, error)
 
+	// UpsertStream creates or updates a stream.
+	UpsertStream(cfg *StreamConfig, opts ...JSOpt) (*StreamInfo, error)
+
 	// DeleteStream deletes a stream.
 	DeleteStream(name string, opts ...JSOpt) error
 
@@ -738,6 +741,15 @@ func (js *js) UpdateStream(cfg *StreamConfig, opts ...JSOpt) (*StreamInfo, error
 		return nil, errors.New(resp.Error.Description)
 	}
 	return resp.StreamInfo, nil
+}
+
+// UpsertStream creates or updates a Stream.
+func (js *js) UpsertStream(cfg *StreamConfig, opts ...JSOpt) (*StreamInfo, error) {
+	info, err := js.AddStream(cfg, opts...)
+	if errors.Is(err, ErrStreamNameAlreadyInUse) {
+		return js.UpdateStream(cfg, opts...)
+	}
+	return info, err
 }
 
 // streamDeleteResponse is the response for a Stream delete request.
