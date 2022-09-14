7d21454551fa09d03ea8f986b06fb4637c4915df
Piotr Piotrowski <piotr@synadia.com>
2022-09-14 16:14:04 +0200

    Update jserrors.go
    Co-authored-by: Waldemar Quevedo <wally@synadia.com>

--- jserrors.go
+++ jserrors.go
@@ -102,7 +102,7 @@ var (
 	ErrRequireServerVersion JetStreamError = &jsError{message: "invalid server version"}
 
 	// ErrConsumerNameMismtch is returned user provides both consumer name and durable name and they are not equal.
-	ErrConsumerNameMismtch JetStreamError = &jsError{message: "consumer name and durable name do not match"}
+	ErrConsumerNameMismatch JetStreamError = &jsError{message: "consumer name and durable name do not match"}
 
 	// DEPRECATED: ErrInvalidDurableName is no longer returned and will be removed in future releases.
 	// Use ErrInvalidConsumerName instead.
