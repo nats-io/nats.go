76b92fb24f1c766c3f772e861776d05ade6e9948
Piotr Piotrowski <piotr@synadia.com>
2023-06-27 12:40:21 +0200

    [IMPROVED] Replace empty interface with any (#1332)

--- nats_test.go
+++ nats_test.go
@@ -64,7 +64,7 @@ func WaitTime(ch chan bool, timeout time.Duration) error {
 	return errors.New("timeout")
 }
 
-func stackFatalf(t *testing.T, f string, args ...interface{}) {
+func stackFatalf(t *testing.T, f string, args ...any) {
 	lines := make([]string, 0, 32)
 	msg := fmt.Sprintf(f, args...)
 	lines = append(lines, msg)
