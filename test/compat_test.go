1ea6ab637580c3a5c0ff6d19662b3372c19f597b
Piotr Piotrowski <piotr@synadia.com>
2023-07-11 14:58:25 +0200

    Improve comparibility tests
    Signed-off-by: Piotr Piotrowski <piotr@synadia.com>

--- test/compat_test.go
+++ test/compat_test.go
@@ -5,7 +5,6 @@ package test
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"testing"
 	"time"
@@ -13,10 +12,13 @@ import (
 	"github.com/nats-io/nats.go"
 )
 
-type objectRequest struct {
-	Config *nats.ObjectMeta `json:"config"`
-	URL    string           `json:"url"`
-	Bucket string           `json:"bucket"`
+type objectStepConfig[T any] struct {
+	Suite   string `json:"suite"`
+	Test    string `json:"test"`
+	Command string `json:"command"`
+	URL     string `json:"url"`
+	Bucket  string `json:"bucket"`
+	Config  T      `json:"config"`
 }
 
 func TestObjectStoreCompatibility(t *testing.T) {
@@ -25,11 +27,15 @@ func TestObjectStoreCompatibility(t *testing.T) {
 		t.Fatalf("Error connecting to NATS: %v", err)
 	}
 	defer nc.Close()
+
+	// setup subscription on which tester will be sending requests
 	sub, err := nc.SubscribeSync("tests.object_store.>")
 	if err != nil {
 		t.Fatalf("Error subscribing to test subject: %v", err)
 	}
 	defer sub.Unsubscribe()
+
+	// 1. Create default bucket
 	init, err := sub.NextMsg(5 * time.Second)
 	if err != nil {
 		t.Fatalf("Error getting message: %v", err)
@@ -45,49 +51,66 @@ func TestObjectStoreCompatibility(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Error creating object store: %v", err)
 	}
+	// send empty response to indicate client is done
 	if err := init.Respond(nil); err != nil {
 		t.Fatalf("Error responding to message: %v", err)
 	}
+	validateStepResult(t, sub)
+
+	// 2. Create bucket with custom config
 	custom, err := sub.NextMsg(5 * time.Second)
 	if err != nil {
 		t.Fatalf("Error getting message: %v", err)
 	}
-	var cfg nats.ObjectStoreConfig
+	var cfg objectStepConfig[*nats.ObjectStoreConfig]
 	if err := json.Unmarshal(custom.Data, &cfg); err != nil {
 		t.Fatalf("Error unmarshalling message: %v", err)
 	}
-	fmt.Printf("%+v\n", cfg)
 
-	os, err = js.CreateObjectStore(&cfg)
+	os, err = js.CreateObjectStore(cfg.Config)
 	if err != nil {
 		t.Fatalf("Error creating object store: %v", err)
 	}
 	if err := custom.Respond(nil); err != nil {
 		t.Fatalf("Error responding to message: %v", err)
 	}
+	validateStepResult(t, sub)
 
+	// 3. Put object
 	objReq, err := sub.NextMsg(5 * time.Second)
 	if err != nil {
 		t.Fatalf("Error getting message: %v", err)
 	}
-	var req objectRequest
-	if err := json.Unmarshal(objReq.Data, &req); err != nil {
+	var putObjectCfg objectStepConfig[*nats.ObjectMeta]
+	if err := json.Unmarshal(objReq.Data, &putObjectCfg); err != nil {
 		t.Fatalf("Error unmarshalling message: %v", err)
 	}
-	os, err = js.ObjectStore(req.Bucket)
+	os, err = js.ObjectStore(putObjectCfg.Bucket)
 	if err != nil {
 		t.Fatalf("Error getting object store: %v", err)
 	}
 	client := http.Client{Timeout: 10 * time.Second, Transport: &http.Transport{DisableKeepAlives: true}}
-	resp, err := client.Get(req.URL)
+	resp, err := client.Get(putObjectCfg.URL)
 	if err != nil {
 		t.Fatalf("Error getting content: %v", err)
 	}
 	defer resp.Body.Close()
-	if _, err := os.Put(req.Config, resp.Body); err != nil {
+	if _, err := os.Put(putObjectCfg.Config, resp.Body); err != nil {
 		t.Fatalf("Error putting object: %v", err)
 	}
 	if err := objReq.Respond(nil); err != nil {
 		t.Fatalf("Error responding to message: %v", err)
+	}
+	validateStepResult(t, sub)
+}
+
+func validateStepResult(t *testing.T, sub *nats.Subscription) {
+	t.Helper()
+	stepEnd, err := sub.NextMsg(5 * time.Second)
+	if err != nil {
+		t.Fatalf("Error getting message: %v", err)
+	}
+	if len(stepEnd.Header["STATUS"]) > 0 {
+		t.Fatalf("Test step failed: %v", stepEnd.Header["STATUS"][0])
 	}
 }
