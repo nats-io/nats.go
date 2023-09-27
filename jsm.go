4d43218266dcf27232ae0431267c597ed24e2ddf
Jean-Noël Moyne <jnmoyne@gmail.com>
2023-09-27 00:20:01 -0700

    [FIXED] Stream create response test for sources being present in the response (#1420)
    Signed-off-by: Jean-Noël Moyne <jnmoyne@gmail.com>
    Signed-off-by: Piotr Piotrowski <piotr@synadia.com>
    Co-authored-by: Piotr Piotrowski <piotr@synadia.com>

--- jsm.go
+++ jsm.go
@@ -817,7 +817,7 @@ func (js *js) AddStream(cfg *StreamConfig, opts ...JSOpt) (*StreamInfo, error) {
 		return nil, ErrStreamSubjectTransformNotSupported
 	}
 	if len(cfg.Sources) != 0 {
-		if len(cfg.Sources) != len(resp.Sources) {
+		if len(cfg.Sources) != len(resp.Config.Sources) {
 			return nil, ErrStreamSourceNotSupported
 		}
 		for i := range cfg.Sources {
@@ -1029,7 +1029,7 @@ func (js *js) UpdateStream(cfg *StreamConfig, opts ...JSOpt) (*StreamInfo, error
 	}
 
 	if len(cfg.Sources) != 0 {
-		if len(cfg.Sources) != len(resp.Sources) {
+		if len(cfg.Sources) != len(resp.Config.Sources) {
 			return nil, ErrStreamSourceNotSupported
 		}
 		for i := range cfg.Sources {
