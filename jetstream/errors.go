bc6c728f62d5a2d8eb232ea638e5e493e7d7c6d3
Piotr Piotrowski <piotr@synadia.com>
2024-04-05 10:28:30 +0200

    [IMPROVED] Documentation for AccountInfo and ErrJetStreamNotEnabled (#1605)
    Signed-off-by: Piotr Piotrowski <piotr@synadia.com>

--- jetstream/errors.go
+++ jetstream/errors.go
@@ -71,6 +71,10 @@ var (
 
 	// ErrJetStreamNotEnabled is an error returned when JetStream is not
 	// enabled.
+	//
+	// Note: This error will not be returned in clustered mode, even if each
+	// server in the cluster does not have JetStream enabled. In clustered mode,
+	// requests will time out instead.
 	ErrJetStreamNotEnabled JetStreamError = &jsError{apiErr: &APIError{ErrorCode: JSErrCodeJetStreamNotEnabled, Description: "jetstream not enabled", Code: 503}}
 
 	// ErrJetStreamNotEnabledForAccount is an error returned when JetStream is
