50e615333dbe1cb7ccf8141ba94d4375bd050de9
Piotr Piotrowski <piotr@synadia.com>
2025-04-09 17:33:30 +0200

    Release v1.41.1 (#1851)
    Signed-off-by: Piotr Piotrowski <piotr@synadia.com>

--- nats.go
+++ nats.go
@@ -47,7 +47,7 @@ import (
 
 // Default Constants
 const (
-	Version                   = "1.41.0"
+	Version                   = "1.41.1"
 	DefaultURL                = "nats://127.0.0.1:4222"
 	DefaultPort               = 4222
 	DefaultMaxReconnect       = 60
