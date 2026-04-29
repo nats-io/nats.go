f1d134bccd1094a609e93d91fb3e41dd25357120
scottf <scottfauerbach@gmail.com>
2026-04-29 12:58:18 -0400

    New Nats Docs, better subjects single wildcard https://github.com/synadia-labs/new-nats.docs/issues/31

--- examples/docs/subjects-single-wildcard/main.go
+++ examples/docs/subjects-single-wildcard/main.go
@@ -13,13 +13,23 @@ func main() {
 
 	// NATS-DOC-START
 	// Subscribe with single token wildcard
-	nc.Subscribe("weather.*.east", func(m *nats.Msg) {
-		fmt.Printf("Received on %s: %s\n", m.Subject, string(m.Data))
+	nc.Subscribe("orders.*.shipped", func(m *nats.Msg) {
+		fmt.Printf("[orders.*.shipped] %s: %s\n", string(m.Data), m.Subject)
+	})
+
+	nc.Subscribe("orders.*.placed", func(m *nats.Msg) {
+		fmt.Printf("[orders.*.placed]  %s: %s\n", string(m.Data), m.Subject)
+	})
+
+	nc.Subscribe("orders.retail.*", func(m *nats.Msg) {
+		fmt.Printf("[orders.retail.*]  %s: %s\n", string(m.Data), m.Subject)
 	})
 
 	// Publish to specific subjects
-	nc.Publish("weather.us.east", []byte("Temperature: 72F"))
-	nc.Publish("weather.eu.east", []byte("Temperature: 18C"))
+	nc.Publish("orders.wholesale.placed", []byte("Order W73737"));
+	nc.Publish("orders.retail.placed", []byte("Order R65432"));
+	nc.Publish("orders.wholesale.shipped", []byte("Order W73001"));
+	nc.Publish("orders.retail.shipped", []byte("Order R65321"));
 	// NATS-DOC-END
 
 	time.Sleep(100 * time.Millisecond)
