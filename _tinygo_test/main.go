62c5aebdb1d36802c781b5ee1f8249f20e99a03d
Piotr Piotrowski <piotr@synadia.com>
2026-05-12 09:29:07 +0200

    tinygo: update test entry-point to full pub/sub smoke test
    Signed-off-by: Piotr Piotrowski <piotr@synadia.com>

--- _tinygo_test/main.go
+++ _tinygo_test/main.go
@@ -3,10 +3,28 @@
 package main
 
 import (
-	nats "github.com/nats-io/nats.go"
 	"fmt"
+
+	nats "github.com/nats-io/nats.go"
 )
 
 func main() {
-	fmt.Println("nats version:", nats.Version)
+	nc, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		panic(err)
+	}
+	defer nc.Close()
+
+	received := make(chan string, 1)
+	nc.Subscribe("tinygo.test", func(m *nats.Msg) {
+		received <- string(m.Data)
+	})
+
+	nc.Publish("tinygo.test", []byte("hello from tinygo"))
+
+	msg := <-received
+	if msg != "hello from tinygo" {
+		panic("wrong message: " + msg)
+	}
+	fmt.Println("OK: pub/sub works under TinyGo native target")
 }
