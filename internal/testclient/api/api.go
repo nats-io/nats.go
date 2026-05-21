4c677bb1a1d2a455dca2ccdaa0b062aa89161d70
Piotr Piotrowski <piotr@synadia.com>
2026-05-21 16:32:31 +0200

    test: vendor tester API types from synadia-labs/testing.go
    Signed-off-by: Piotr Piotrowski <piotr@synadia.com>

---
+++ internal/testclient/api/api.go
@@ -0,0 +1,112 @@
+// Copyright 2026 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package api
+
+import "time"
+
+type CreateServerRequest struct {
+	JetStream   bool              `json:"jetstream"`
+	Description string            `json:"description,omitempty"`
+	Snippets    map[string]string `json:"snippets,omitempty"`
+	Template    string            `json:"template,omitempty"`
+}
+
+type CreateClusterRequest struct {
+	Servers     int               `json:"servers"`
+	JetStream   bool              `json:"jetstream"`
+	Description string            `json:"description,omitempty"`
+	Snippets    map[string]string `json:"snippets,omitempty"`
+	Template    string            `json:"template,omitempty"`
+}
+
+type CreateSuperClusterRequest struct {
+	Servers     int               `json:"servers"`
+	Clusters    int               `json:"clusters"`
+	JetStream   bool              `json:"jetstream" yaml:"jetstream"`
+	Description string            `json:"description,omitempty"`
+	Snippets    map[string]string `json:"snippets,omitempty"`
+	Template    string            `json:"template,omitempty"`
+}
+
+type ManagedServer struct {
+	Name    string         `json:"name"`
+	Cluster string         `json:"cluster"`
+	Port    int            `json:"port"`
+	Ports   map[string]int `json:"ports,omitempty"`
+	URL     string         `json:"url,omitempty"`
+	Running bool           `json:"running"`
+}
+
+type CreateResponse struct {
+	ID          string           `json:"id"`
+	Description string           `json:"description,omitempty"`
+	Kind        string           `json:"kind"`
+	Servers     []*ManagedServer `json:"servers"`
+}
+
+type DestroyRequest struct {
+	InstanceID string `json:"instance_id"`
+}
+
+type DestroyResponse struct {
+	Destroyed bool `json:"destroyed"`
+}
+
+type InstanceSummary struct {
+	ID          string    `json:"id"`
+	Description string    `json:"description,omitempty"`
+	Kind        string    `json:"kind"`
+	Cluster     string    `json:"cluster,omitempty"`
+	Servers     int       `json:"servers"`
+	Created     time.Time `json:"created"`
+}
+
+type ListResponse struct {
+	Instances []InstanceSummary `json:"instances"`
+}
+
+type ResetResponse struct {
+	Shutdown bool `json:"shutdown"`
+}
+
+type StartServerRequest struct {
+	Name string `json:"name"`
+}
+
+type StopServerRequest struct {
+	Name string `json:"name"`
+}
+
+type StopServerResponse struct {
+	Shutdown bool `json:"shutdown"`
+}
+
+type StartServerResponse struct {
+	Started bool `json:"shutdown"`
+}
+
+type StatusRequest struct {
+	InstanceID string `json:"instance_id,omitempty"`
+}
+
+type InstanceStatus struct {
+	ID          string          `json:"id"`
+	Description string          `json:"description,omitempty"`
+	Kind        string          `json:"kind"`
+	Servers     []ManagedServer `json:"servers"`
+}
+
+type StatusResponse struct {
+	Instances []InstanceStatus `json:"instances"`
+}
