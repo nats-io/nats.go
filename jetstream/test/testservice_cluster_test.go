aecd1fac78702a2fa39e6109ebba579025b7da7f
Piotr Piotrowski <piotr@synadia.com>
2026-05-21 18:07:11 +0200

    test: PoC 3-node cluster test against tester
    Signed-off-by: Piotr Piotrowski <piotr@synadia.com>

---
+++ jetstream/test/testservice_cluster_test.go
@@ -0,0 +1,75 @@
+// Copyright 2026 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build testservice
+
+package test
+
+import (
+	"testing"
+
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/internal/testclient/testservice"
+	"github.com/nats-io/nats.go/jetstream"
+)
+
+func TestTesterJSCluster(t *testing.T) {
+	withTesterJSCluster(t, 3, func(t *testing.T, nc *nats.Conn, js jetstream.JetStream, inst *testservice.Instance) {
+		ctx := withTesterCtx(t)
+
+		if len(inst.Servers) != 3 {
+			t.Fatalf("expected 3 servers in cluster, got %d", len(inst.Servers))
+		}
+
+		s, err := js.CreateStream(ctx, jetstream.StreamConfig{
+			Name:     "POC2",
+			Subjects: []string{"poc2.*"},
+			Replicas: 3,
+		})
+		if err != nil {
+			t.Fatalf("CreateStream: %v", err)
+		}
+
+		for i := range 10 {
+			if _, err := js.Publish(ctx, "poc2.x", []byte{byte('0' + i)}); err != nil {
+				t.Fatalf("Publish %d: %v", i, err)
+			}
+		}
+
+		c, err := s.CreateOrUpdateConsumer(ctx, jetstream.ConsumerConfig{
+			Durable:   "poc2c",
+			AckPolicy: jetstream.AckExplicitPolicy,
+		})
+		if err != nil {
+			t.Fatalf("CreateOrUpdateConsumer: %v", err)
+		}
+
+		msgs, err := c.Fetch(10)
+		if err != nil {
+			t.Fatalf("Fetch: %v", err)
+		}
+		got := 0
+		for msg := range msgs.Messages() {
+			if err := msg.Ack(); err != nil {
+				t.Fatalf("Ack: %v", err)
+			}
+			got++
+		}
+		if msgs.Error() != nil {
+			t.Fatalf("Fetch error: %v", msgs.Error())
+		}
+		if got != 10 {
+			t.Fatalf("expected 10 fetched msgs, got %d", got)
+		}
+	})
+}
